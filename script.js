817c96efead09fdc6791ba19a4b6f8bccbf3a87d
mehdi0283 <malekeym@gmail.com>
2020-10-18 18:12:04 +0330

    pianoView

---
+++ script.js
@@ -0,0 +1,13 @@
+const note = document.querySelector('.nowplaying');
+
+
+window.addEventListener('keydown' , function(e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+ 
+    if(! key) return;
+
+    const keyNote = key.getAttribute('data-note');
+ 
+    note.innerHTML = keyNote;
+
+});
